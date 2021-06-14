55a777969b4dfb6616fcbf3e614a5bbe6b972d5c
SanShamor <sanshamor@mail.ru>
2021-06-14 21:04:37 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Александр on 14.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
